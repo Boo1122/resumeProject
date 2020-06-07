7075fefd0a0e0a852dca75d9c4d3c507265d90c8
Boo1122 <you@example.com>
2020-06-07 04:39:40 +0300

    start project

---
+++ resume/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.css'
+
+function App() {
+  return (
+    <div className="App">
+      <hr className='line' />
+      <div className='left'></div>
+      <div className='top'></div>
+      <div className='bottom'></div>
+      <div className='right'></div>
+    </div>
+  );
+}
+
+export default App;
